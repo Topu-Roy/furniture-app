524f4a064d12ad70893e2f4b819ee4835dc7552d
Topu-Roy <topuroy143333@gmail.com>
2024-03-11 19:04:09 +0600

    Fix

--- src/app/shop/filterByPrice.tsx
+++ src/app/shop/filterByPrice.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Heading } from '@/components'
 import { Input } from '@/components/ui/input'
 import { Slider } from '@/components/ui/slider'
@@ -5,18 +6,17 @@ import React, { useState } from 'react'
 
 export default function FilterByPrice() {
     const [sliderValue, setSliderValue] = useState(500);
-    console.log(sliderValue)
     return (
         <div className="flex flex-col gap-4">
             <Heading size='md'>Filter by price</Heading>
             <div className="flex flex-row justify-center items-center gap-1">
                 <div className="flex justify-center items-center gap-2 bg-gray-50 border-[1px] border-black pl-1.5 ">
-                    <span className=''>Min</span>
-                    <Input placeholder='$500' className='rounded-[0px] flex-1' />
+                    <span className=''>Min $:</span>
+                    <Input placeholder='500' className='rounded-[0px] flex-1' />
                 </div>
                 <div className="flex justify-center items-center gap-2 bg-gray-50 border-[1px] border-black pl-1.5 ">
-                    <span className=''>Max</span>
-                    <Input placeholder='$2000' className='rounded-[0px] flex-1' />
+                    <span className=''>Max $:</span>
+                    <Input placeholder='2000' className='rounded-[0px] flex-1' />
                 </div>
 
             </div>
