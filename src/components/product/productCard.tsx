2e0a106171aa37fc2da6755f0414a44c1edcc103
Topu-Roy <topuroy143333@gmail.com>
2024-03-15 01:03:27 +0600

    Dep - deleted heroicons and added react-icons

--- src/components/product/productCard.tsx
+++ src/components/product/productCard.tsx
@@ -57,11 +57,16 @@ export default function Product({
             ) : null}
 
             <div className="flex flex-col items-center justify-start w-[96%] gap-5">
-              {heartIcon ? (
-                <Button color="black_900" className="w-10 shadow-xs h-10 rounded-full bg-slate-500/40 p-2 hover:bg-rose-600/50">
-                  <Image height={100} width={100} alt="icon" src={`/${heartIcon}`} />
-                </Button>
-              ) : null}
+
+              <Button color="black_900" className="w-10 shadow-xs h-10 text-red-700 rounded-full bg-slate-500/40 p-2 hover:bg-rose-600/50">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                </svg>
+              </Button>
+
+
+
+
               {cartIcon ? (
                 <Button color="yellow_100" className="w-10 shadow-xs h-10 rounded-full bg-slate-300/40 p-2 hover:bg-green-600/80">
                   <Image height={100} width={100} alt="icon" src={`/${cartIcon}`} />
