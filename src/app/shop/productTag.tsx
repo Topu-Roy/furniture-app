15b53fe6d552be62a4c65a0b56c632d797ca579e
Topu-Roy <topuroy143333@gmail.com>
2024-03-18 22:41:39 +0600

    Feat - Tags counter

--- src/app/shop/productTag.tsx
+++ src/app/shop/productTag.tsx
@@ -1,29 +1,59 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Tag, useShopStore } from '@/zustand/shop/shopStore'
-import { cn } from '@/lib/utils'
+import { cn, scrollToTop } from '@/lib/utils'
 import HeadingAndReset from './headingAndReset'
 
+type ProductTagsType = {
+    tag: Tag,
+    quantity: number
+}
+
 export default function ProductTag() {
-    const { selectedTag } = useShopStore();
-    const productTags: Tag[] = [
-        "All",
-        "Minimalistic",
-        "Modern",
-        "Stylish",
-        "Elegant",
-        "Ambient",
-        "Luxurious",
-    ]
+    const { selectedTag, productsBackup } = useShopStore();
+    const [productTags, setProductTags] = useState<ProductTagsType[]>([])
+
+    useEffect(() => {
+        const tags: ProductTagsType[] = [
+            {
+                // @ts-ignore
+                tag: "All",
+                quantity: productsBackup.length
+            },
+            {
+                tag: "Minimalistic",
+                quantity: productsBackup.filter(item => item.tag === 'Minimalistic').length
+            },
+            {
+                tag: "Modern",
+                quantity: productsBackup.filter(item => item.tag === 'Modern').length
+            },
+            {
+                tag: "Stylish",
+                quantity: productsBackup.filter(item => item.tag === 'Stylish').length
+            },
+            {
+                tag: "Elegant",
+                quantity: productsBackup.filter(item => item.tag === 'Elegant').length
+            },
+            {
+                tag: "Ambient",
+                quantity: productsBackup.filter(item => item.tag === 'Ambient').length
+            },
+            {
+                tag: "Luxurious",
+                quantity: productsBackup.filter(item => item.tag === 'Luxurious').length
+            }
+        ]
+
+        setProductTags(tags)
+    }, [productsBackup])
 
     function handleReset() {
         useShopStore.setState({ selectedTag: 'All' });
 
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        })
+        scrollToTop();
     }
 
     return (
@@ -31,18 +61,18 @@ export default function ProductTag() {
             <HeadingAndReset title='Filter By Tag' handleReset={handleReset} />
             <div className="flex flex-col items-start justify-start w-full gap-4">
                 <div className="flex flex-row justify-start flex-wrap gap-2">
-                    {productTags.map(tag => (
+                    {productTags.map(item => (
                         <Button
-                            onClick={() => useShopStore.setState({ selectedTag: tag })}
+                            onClick={() => useShopStore.setState({ selectedTag: item.tag })}
                             size="lg"
                             variant={"link"}
                             className={cn("cursor-pointer tracking-[-0.50px] px-3 py-1 bg-slate-200/90 rounded-full text-gray-700/90 min-w-[3rem]",
                                 {
-                                    "ring-[2px] ring-black/20 text-black": tag === selectedTag
+                                    "ring-[2px] ring-black/20 text-black": item.tag === selectedTag
                                 })}
-                            key={tag}
+                            key={item.tag}
                         >
-                            {tag}
+                            {item.tag} ({item.quantity})
                         </Button>
                     ))}
                 </div>
