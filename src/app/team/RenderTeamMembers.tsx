0790d753867b41a629eee3cd1cac0bfbcca33332
Topu-Roy <topuroy143333@gmail.com>
2024-03-21 15:42:19 +0600

    Feat - Team page done

---
+++ src/app/team/RenderTeamMembers.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { Button } from "@/components";
+import Image from "next/image";
+import React from "react";
+import { useEffect, useState } from "react";
+import { FaFacebookF, FaGithub } from "react-icons/fa6";
+import { IoLogoWhatsapp } from "react-icons/io";
+
+type TeamMember = {
+  id: number;
+  role: string;
+  name: string;
+  imageUrl: string;
+};
+
+type props = {
+  teamMembers: TeamMember[];
+};
+
+export default function RenderTeamMembers(props: props) {
+  const [teamMembers, setTeamMembers] = useState<TeamMember[]>([]);
+
+  useEffect(() => {
+    setTeamMembers(props.teamMembers);
+  }, [props.teamMembers]);
+
+  <FaFacebookF />;
+  <IoLogoWhatsapp />;
+  <FaGithub />;
+
+  return (
+    <div className="mx-auto grid max-w-7xl grid-cols-3 gap-4 pb-20 pt-10">
+      {teamMembers.map((member) => (
+        <div className="w-full space-y-4 rounded-xl border p-4 shadow">
+          <div className="group relative z-10 max-w-sm overflow-hidden rounded-xl">
+            <Image
+              className="aspect-square w-full transition-all duration-300 group-hover:scale-105"
+              src={member.imageUrl}
+              height={1024}
+              width={1024}
+              alt={member.name + " " + member.role}
+            />
+
+            <div className="pointer-events-none absolute bottom-0 left-0 z-[19] h-[40%] w-full translate-y-[150%] select-none bg-gradient-to-t from-white/50 to-transparent ring-0 transition-all duration-300 group-hover:translate-y-0" />
+            <div className="absolute bottom-[3%] left-0 z-20 flex w-full translate-y-[150%] flex-row items-center justify-around gap-2 ring-0 transition-all duration-300 group-hover:translate-y-0">
+              <div className="flex flex-row items-center justify-between gap-3">
+                <Button className="flex items-center justify-center rounded-full bg-white p-2 hover:bg-gray-400/90">
+                  <FaFacebookF size={25} className="text-blue-600" />
+                </Button>
+                <Button className="flex items-center justify-center rounded-full bg-white p-2 hover:bg-gray-400/90">
+                  <IoLogoWhatsapp size={25} className="text-green-600" />
+                </Button>
+                <Button className="flex items-center justify-center rounded-full bg-white p-2 hover:bg-gray-400/90">
+                  <FaGithub size={25} className="text-black" />
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-start gap-2">
+            <span className="text-xl font-bold tracking-wide text-black/85">
+              {member.name}
+            </span>
+            <span className="font-medium text-black/70">({member.role})</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,67 +1,28 @@
-"use client"
 import { Heading, Text } from "@/components";
 import Footer from "@/components/Footer";
-import Header from "@/components/NavBar";
-import TeamCardteam from "@/components/TeamCardteam";
+import NavBar from "@/components/NavBar";
+import { teamMembers } from "../aboutUs/teamMembersArray";
+import RenderTeamMembers from "./RenderTeamMembers";
 
 export default function TeamPage() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-start w-full gap-[98px] bg-gray-50">
-        <Header className="flex justify-center items-center w-full p-[35px] bg-white-A700" />
-        <div className="flex flex-row justify-center w-full">
-          <div className="flex flex-row justify-center w-full">
-            <div className="flex flex-col items-center justify-start w-full gap-[50px] max-w-[1290px]">
-              <div className="flex flex-col items-center justify-start w-full gap-3">
-                <Heading size="3xl" as="h1" className="tracking-[-0.50px] text-center">
-                  Meet Our Team
-                </Heading>
-                <Text size="lg" className="!text-gray-500 tracking-[-0.50px] text-center leading-[35px]">
-                  We write various things related to furniture, from tips and what things <br />I need to pay attention
-                  to when choosing furniture
-                </Text>
-              </div>
-              <div className="justify-center w-full gap-5 grid-cols-3 grid min-h-[auto]">
-                <TeamCardteam
-                  bxllinkedinone="images/img_bxl_linkedin_1.svg"
-                  bxlfacebookone="images/img_bxl_facebook.svg"
-                  bxltwitterone="images/img_bxl_twitter.svg"
-                  className="flex flex-col items-center justify-start w-full gap-7"
-                />
-                <TeamCardteam
-                  image="images/img_rectangle_1487_450x416.png"
-                  makejhane="Jinny Owen"
-                  financemanager="Marketing Manager"
-                  className="flex flex-col items-center justify-start w-full gap-[30px]"
-                />
-                <TeamCardteam
-                  image="images/img_rectangle_1487_1.png"
-                  makejhane="Mia Lobey"
-                  financemanager="Accountant"
-                  className="flex flex-col items-center justify-start w-full gap-[30px]"
-                />
-                <TeamCardteam
-                  image="images/img_rectangle_1487_2.png"
-                  className="flex flex-col items-center justify-start w-full gap-7"
-                />
-                <TeamCardteam
-                  image="images/img_rectangle_1487_3.png"
-                  makejhane="Jinny Owen"
-                  financemanager="Marketing Manager"
-                  className="flex flex-col items-center justify-start w-full gap-[30px]"
-                />
-                <TeamCardteam
-                  image="images/img_rectangle_1487_4.png"
-                  makejhane="Mia Lobey"
-                  financemanager="Accountant"
-                  className="flex flex-col items-center justify-start w-full gap-[30px]"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <Footer className="flex flex-col items-center justify-center w-full" />
-      </div>
-    </>
+    <div className="mt-[5rem] bg-gray-50 pt-10">
+      <NavBar />
+
+      <Heading size="3xl" className="text-center tracking-[-0.50px]">
+        Meet Our Team
+      </Heading>
+      <Text
+        size="lg"
+        className="text-center leading-[35px] tracking-[-0.50px] !text-gray-500"
+      >
+        We write various things related to furniture, from tips and what things{" "}
+        <br />I need to pay attention to when choosing furniture
+      </Text>
+
+      <RenderTeamMembers teamMembers={teamMembers} />
+
+      <Footer className="flex w-full flex-col items-center justify-center" />
+    </div>
   );
 }
