a709198e72a0cadc26bb0982f13e13344959416d
Topu-Roy <topuroy143333@gmail.com>
2024-03-30 18:31:37 +0600

    Feat - Excluded the isSelected field from the localstorage

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,18 +3,50 @@ import React, { useEffect, useState } from "react";
 import { Heading, Text, Input, Img } from "../../components";
 import { CartProductType, useCartStore } from "@/zustand/cart/cartStore";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { TiMinus, TiPlus } from "react-icons/ti";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function CartPage() {
-  const { products, removeFromCart, increaseQuantity, decreaseQuantity } =
-    useCartStore();
+  const {
+    products,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    changeSelected,
+  } = useCartStore();
   const [productsToRender, setProductsToRender] = useState<CartProductType[]>(
     [],
   );
+  const [checkoutPrice, setCheckoutPrice] = useState<number | null>(null);
 
   useEffect(() => {
     setProductsToRender(products);
     console.log(products);
   }, [products]);
+
+  useEffect(() => {
+    let totalPriceForSelectedProducts = 0;
+
+    const selectedProductsArr = products.filter(
+      (item) => item.isSelected == true,
+    );
+
+    selectedProductsArr.forEach((item) => {
+      totalPriceForSelectedProducts =
+        totalPriceForSelectedProducts + item.price;
+    });
+
+    setCheckoutPrice(totalPriceForSelectedProducts);
+
+    console.log(products);
+  }, [products]);
+
+  useEffect(() => {
+    products.forEach((item) => {
+      changeSelected(item.id, false);
+    });
+  }, []);
 
   function handleRemove(id: number) {
     removeFromCart(id);
@@ -25,219 +57,133 @@ export default function CartPage() {
   function handleDecrease(id: number) {
     decreaseQuantity(id);
   }
+  function handleSelect(id: number) {
+    const selectedProduct = products.find((item) => item.id === id);
+    changeSelected(id, selectedProduct?.isSelected == false ? true : false);
+  }
 
   return (
     <>
-      <div className="mt-[5rem]">
-        {productsToRender.map((item) => (
-          <div key={item.id} className="">
-            <div className="">{item.productTitle}</div>
-            <Button onClick={() => handleRemove(item.id)}>Delete</Button>
-            <Button onClick={() => handleIncrease(item.id)}>Add</Button>
-            <Button onClick={() => handleDecrease(item.id)}>Decrease</Button>
-          </div>
-        ))}
-      </div>
-      {/* <div className="flex w-full flex-col items-center justify-start gap-[100px] bg-gray-50">
-        <div className="flex w-full flex-col items-center justify-start">
-          <header className="bg-white-A700 flex w-full flex-row items-center justify-between p-[35px]">
-            <Img
-              src="images/img_frame_146.svg"
-              alt="image"
-              className="ml-10 h-[30px]"
-            />
-            <div className="flex w-[37%] flex-row items-center justify-between">
-              <div className="flex flex-row items-center justify-start gap-2.5">
-                <Text size="md" className="!text-black-900 tracking-[-0.50px]">
-                  Home
-                </Text>
-                <Img
-                  src="images/img_arrow_down.svg"
-                  alt="arrowdown_one"
-                  className="h-6 w-6"
+      <Heading size="3xl" className="mt-[5rem] py-8 text-center">
+        My Cart
+      </Heading>
+      <div className="mx-auto flex max-w-7xl items-start justify-between gap-2">
+        <div className="w-full space-y-2">
+          {productsToRender.map((item) => (
+            <div
+              key={item.id}
+              className="grid w-full grid-cols-8 gap-2 bg-zinc-100"
+            >
+              <div className="col-span-1 ">
+                <Checkbox
+                  onClick={() => handleSelect(item.id)}
+                  checked={item.isSelected}
+                  id={item.id.toString()}
+                />
+                <label
+                  htmlFor={item.id.toString()}
+                  onClick={() => handleSelect(item.id)}
+                >
+                  {item.isSelected ? "Selected" : "Select"}
+                </label>
+              </div>
+              <div className="col-span-2 max-w-[10rem] overflow-hidden rounded-md">
+                <Image
+                  src={item.image}
+                  height={1024}
+                  width={1024}
+                  alt={item.productTitle}
                 />
               </div>
-              <Text size="md" className="!text-black-900 tracking-[-0.50px]">
-                Shop
-              </Text>
-              <Text size="md" className="!text-black-900 tracking-[-0.50px]">
-                Blog
-              </Text>
-              <Text size="md" className="!text-black-900 tracking-[-0.50px]">
-                About
-              </Text>
-              <Text size="md" className="!text-black-900 tracking-[-0.50px]">
-                Contact
-              </Text>
-              <Text size="md" className="!text-black-900 tracking-[-0.50px]">
-                Team
-              </Text>
-            </div>
-            <div className="mr-10 flex w-auto flex-row justify-between gap-[30px]">
-              <Img
-                src="images/img_search.svg"
-                alt="search_one"
-                className="h-6 w-6"
-              />
-              <Img
-                src="images/img_list_black_900.svg"
-                alt="list_one"
-                className="h-6 w-6"
-              />
-              <Img
-                src="images/img_list.svg"
-                alt="list_three"
-                className="h-6 w-6"
-              />
-            </div>
-          </header>
-          <div className="flex w-full flex-row justify-center px-14 pt-[75px]">
-            <div className="flex w-full max-w-[1290px] flex-col items-center justify-start gap-[49px]">
-              <Heading size="3xl" className="text-center tracking-[-0.50px]">
-                Your Cart
-              </Heading>
-              <div className="flex w-full flex-row items-center justify-between">
-                <div className="flex w-[64%] flex-col gap-[30px]">
-                  <div className="flex w-full flex-row items-center justify-between">
-                    <div className="flex w-[88%] flex-row items-center justify-between">
-                      <div className="flex w-[62%] flex-row items-center justify-start gap-5">
-                        <Img
-                          src="images/img_rectangle_1480.png"
-                          alt="image"
-                          className="w-[120px] object-cover"
-                        />
-                        <div className="flex w-[68%] flex-col items-start justify-start gap-[15px]">
-                          <Heading className="!font-bold !leading-[35px] tracking-[-0.50px]">
-                            Complete set of sofa, pillows and bed sheets
-                          </Heading>
-                          <Heading className="!font-poppins !font-bold tracking-[-0.50px] !text-blue_gray-900_01">
-                            $ 75.00
-                          </Heading>
-                        </div>
-                      </div>
-                      <div className="border-black-900 flex w-[17%] flex-row justify-center border border-solid p-1">
-                        <div className="mx-2.5 flex w-4/5 flex-row items-center justify-between py-[5px]">
-                          <Button size="xs" className="w-6">
-                            <Img src="images/defaultNoData.png" />
-                          </Button>
-                          <Text
-                            size="md"
-                            className="!text-black-900 tracking-[-0.50px]"
-                          >
-                            1
-                          </Text>
-                          <Button size="xs" className="w-6">
-                            <Img src="images/img_bx_plus_circle_1.svg" />
-                          </Button>
-                        </div>
-                      </div>
-                      <Heading size="md" className="tracking-[-0.50px]">
-                        $ 75.00
-                      </Heading>
-                    </div>
-                    <Button color="gray_50_01" size="7xl" className="w-[50px]">
-                      <Img src="images/img_frame_48096032.svg" />
+              <div className="col-span-3 space-y-2">
+                <Text size="lg" className="font-semibold">
+                  {item.productTitle}
+                </Text>
+                <Text size="md" className="">
+                  Color: {item.color}
+                </Text>
+                <Text size="md" className="">
+                  Category: <span className="">{item.category}</span>
+                </Text>
+                <Text size="md" className="">
+                  Price:{" "}
+                  <span className="text-lg font-semibold">$ {item.price}</span>
+                </Text>
+              </div>
+              <div className="col-span-2">
+                <div className="flex items-center justify-center gap-2">
+                  <Text>Quantity</Text>
+                  <div className="flex h-14 items-center justify-center gap-5 rounded-md border border-black p-2">
+                    <Button
+                      onClick={() => handleDecrease(item.id)}
+                      className="flex h-8 w-8 items-center justify-center rounded-full p-2"
+                      variant={"outline"}
+                    >
+                      <TiMinus size={20} className="text-black" />
                     </Button>
-                  </div>
-                  <div className="flex w-full flex-row items-center justify-between">
-                    <div className="flex w-[54%] flex-row items-center justify-start gap-5">
-                      <Img
-                        src="images/img_rectangle_1480.png"
-                        alt="image"
-                        className="w-[120px] object-cover"
-                      />
-                      <div className="flex w-[68%] flex-col items-start justify-start gap-[15px]">
-                        <Heading className="!font-bold tracking-[-0.50px]">
-                          Teak wood chair
-                        </Heading>
-                        <Heading className="!font-poppins !font-bold tracking-[-0.50px] !text-blue_gray-900_01">
-                          $ 24.00
-                        </Heading>
-                      </div>
-                    </div>
-                    <div className="border-black-900 flex w-[15%] flex-row justify-center border border-solid p-1">
-                      <div className="mx-2.5 flex w-4/5 flex-row items-center justify-between py-[5px]">
-                        <Img
-                          src="images/img_bx_minus_circle.svg"
-                          alt="image_one"
-                          className="h-6 w-6"
-                        />
-                        <Text
-                          size="md"
-                          className="!text-black-900 tracking-[-0.50px]"
-                        >
-                          1
-                        </Text>
-                        <Img
-                          src="images/img_bx_plus_circle_1_blue_gray_900_01.svg"
-                          alt="image_two"
-                          className="h-6 w-6"
-                        />
-                      </div>
-                    </div>
-                    <Heading size="md" className="tracking-[-0.50px]">
-                      $ 75.00
-                    </Heading>
-                    <Button color="gray_50_01" size="7xl" className="w-[50px]">
-                      <Img src="images/img_frame_48096032.svg" />
+                    <span className="">{item.quantity}</span>
+                    <Button
+                      onClick={() => handleIncrease(item.id)}
+                      className="flex h-8 w-8 items-center justify-center rounded-full p-2"
+                      variant={"outline"}
+                    >
+                      <TiPlus size={20} className="text-black" />
                     </Button>
                   </div>
                 </div>
-                <div className="flex w-[33%] flex-col items-start justify-end gap-7 bg-gray-50_01 p-[27px]">
-                  <Heading className="mt-[9px] !font-bold tracking-[-0.50px]">
-                    Cart Total
-                  </Heading>
-                  <div className="flex w-full flex-col items-center justify-start gap-5">
-                    <div className="flex w-full flex-row items-center justify-between py-0.5">
-                      <Text
-                        size="lg"
-                        className="!font-raleway tracking-[-0.50px] !text-gray-500"
-                      >
-                        Subtotal
-                      </Text>
-                      <Heading className="!font-poppins tracking-[-0.50px]">
-                        $ 99.00
-                      </Heading>
-                    </div>
-                    <div className="flex w-full flex-row items-center justify-center">
-                      <Input
-                        size="sm"
-                        name="your_voucher"
-                        placeholder="Your Voucher"
-                        className="!text-black-900_3f w-[73%]"
-                      />
-                      <Button
-                        size="5xl"
-                        className="min-w-[98px] font-semibold tracking-[-0.50px] !text-yellow-100"
-                      >
-                        Apply
-                      </Button>
-                    </div>
-                  </div>
-                  <div className="bg-black-900 h-px w-full" />
-                  <div className="flex w-full flex-row items-center justify-between py-0.5">
-                    <Text
-                      size="lg"
-                      className="!font-raleway tracking-[-0.50px] !text-gray-500"
-                    >
-                      Total
-                    </Text>
-                    <Heading className="!font-poppins tracking-[-0.50px]">
-                      $ 99.00
-                    </Heading>
-                  </div>
-                  <Button
-                    size="8xl"
-                    className="mb-1 w-full font-semibold tracking-[-0.50px] !text-yellow-100"
-                  >
-                    Checkout Now
-                  </Button>
+                <div className="flex items-center justify-center gap-2">
+                  <Text>Total price:</Text>
+                  <Text>{item.price}</Text>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-      </div> */}
+        <div className="flex w-[33%] flex-col items-start justify-end gap-7 bg-gray-50_01 p-[27px]">
+          <Heading className="mt-[9px] !font-bold tracking-[-0.50px]">
+            Total for selected items
+          </Heading>
+          <div className="flex w-full flex-col items-center justify-start gap-5">
+            <div className="flex w-full flex-row items-center justify-between py-0.5">
+              <Text
+                size="lg"
+                className="!font-raleway tracking-[-0.50px] !text-gray-500"
+              >
+                Subtotal
+              </Text>
+              <Heading className="!font-poppins tracking-[-0.50px]">
+                $ 99.00
+              </Heading>
+            </div>
+            <div className="flex w-full flex-row items-center justify-center">
+              <Input
+                size="sm"
+                name="your_voucher"
+                placeholder="Your Voucher"
+                className="!text-black-900_3f w-[73%]"
+              />
+              <Button className="min-w-[98px] font-semibold tracking-[-0.50px] !text-yellow-100">
+                Apply
+              </Button>
+            </div>
+          </div>
+          <div className="bg-black-900 h-px w-full" />
+          <div className="flex w-full flex-row items-center justify-between py-0.5">
+            <Text
+              size="lg"
+              className="!font-raleway tracking-[-0.50px] !text-gray-500"
+            >
+              Total
+            </Text>
+            <Heading className="!font-poppins tracking-[-0.50px]">
+              $ {checkoutPrice}
+            </Heading>
+          </div>
+          <Button className="mb-1 w-full font-semibold tracking-[-0.50px] !text-yellow-100">
+            Checkout Now
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
--- src/zustand/cart/cartStore.ts
+++ src/zustand/cart/cartStore.ts
@@ -9,26 +9,29 @@ export type CartProductType = {
     id: number;
     productTitle: string;
     image: string;
-    price: number | undefined;
+    price: number;
     status?: "new" | "popular" | "out of stock";
     category: Category;
     tag: Tag;
     color: Color;
-    quantity: number
+    quantity: number;
+    isSelected: boolean
 };
 
 type UseShopStoreType = {
-    products: CartProductType[];
+    products: CartProductType[]
     addToCart: (product: CartProductType) => void
     removeFromCart: (id: number) => void
     increaseQuantity: (id: number) => void
     decreaseQuantity: (id: number) => void
+    changeSelected: (id: number, selection: boolean) => void
 };
 
 export const useCartStore = create<UseShopStoreType>()(
     persist(
         (set, get) => ({
             products: [],
+            isSelected: false,
             addToCart: (product) => set(() => {
                 const isExist = get().products.find(item => item.id === product.id);
 
@@ -48,10 +51,19 @@ export const useCartStore = create<UseShopStoreType>()(
                 products: get().products.map(item => (
                     item.id === id ? { ...item, quantity: item.quantity > 1 ? item.quantity - 1 : item.quantity } : item
                 ))
-            }))
-        }), {
-        name: "cart-storage",
-        storage: createJSONStorage(() => sessionStorage)
-    }
+            })),
+            changeSelected: (id, selection) => set(() => ({
+                products: get().products.map(item => (
+                    item.id === id ? { ...item, isSelected: selection } : item
+                ))
+            })),
+        }),
+        {
+            name: "cart-storage",
+            storage: createJSONStorage(() => sessionStorage),
+            partialize: (state) => ({
+                products: state.products.map(({ isSelected, ...rest }) => rest)             //* Excluding the isSelected field from the local storage 
+            }),
+        }
     )
 );
