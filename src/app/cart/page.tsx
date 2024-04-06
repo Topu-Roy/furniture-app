58c9679dd737794e67e8c9e3c38be50231805c42
Topu-Roy <topuroy143333@gmail.com>
2024-04-06 14:31:53 +0600

    Cart ui changes

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -12,6 +12,15 @@ import {
 } from "react-icons/io5";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { cn } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function CartPage() {
   const { products, removeFromCart, increaseQuantity, decreaseQuantity } =
@@ -50,6 +59,23 @@ export default function CartPage() {
     decreaseQuantity(id);
   }
 
+  function handleSelectAll() {
+    useCartStore.setState({
+      products: products.map((item) => ({
+        ...item,
+        isSelected: true,
+      })),
+    });
+  }
+  function handleReset() {
+    useCartStore.setState({
+      products: products.map((item) => ({
+        ...item,
+        isSelected: false,
+      })),
+    });
+  }
+
   function handleCheck(id: number) {
     useCartStore.setState({
       products: products.map((item) =>
@@ -59,53 +85,102 @@ export default function CartPage() {
   }
 
   return (
-    <>
-      <Heading size="3xl" className="mt-[5rem] py-8 text-center">
+    <div className="bg-stone-200">
+      <Heading size="3xl" className="mt-[5rem] text-center">
         My Cart
       </Heading>
 
-      <div className="mx-auto flex max-w-7xl items-start justify-between gap-2">
+      <Text size="md" className="pb-8 pt-5 text-center text-rose-500 underline">
+        * Select products to Checkout
+      </Text>
+
+      <div className="mx-auto flex max-w-7xl items-start justify-start gap-2 pb-4">
+        <Button onClick={() => handleSelectAll()} className="hover:scale-105">
+          Select All
+        </Button>
+        <Button onClick={() => handleReset()} className="hover:scale-105">
+          Reset
+        </Button>
+      </div>
+
+      <div className="mx-auto flex max-w-7xl items-start justify-between gap-2 pb-10">
         <div className="w-full space-y-2">
           {productsToRender.map((item) => (
             <div
               key={item.id}
               className={cn(
-                "grid w-full grid-cols-8 gap-2 rounded-lg bg-zinc-100 p-2",
+                "grid w-full grid-cols-8 gap-2 rounded-lg bg-zinc-100 p-2 shadow-md",
                 {
-                  "ring-2 ring-neutral-500": item.isSelected,
+                  "ring-2 ring-neutral-500/50": item.isSelected,
                 },
               )}
             >
               <div className="relative col-span-1 flex flex-col items-center justify-center">
                 <label className="flex flex-1 items-center justify-center">
                   <input
-                    className="h-[2.5rem] w-[2.5rem] opacity-0"
+                    className="peer h-[2.5rem] w-[2.5rem] opacity-0"
                     type="checkbox"
                     checked={item.isSelected || false}
                     onChange={() => handleCheck(item.id)}
                   />
                   <Button
                     variant="ghost"
-                    className="pointer-events-none absolute rounded-full p-0 shadow-sm"
+                    className={cn(
+                      "pointer-events-none absolute h-[3.5rem] w-[3.5rem] bg-neutral-200 p-0 text-neutral-700 shadow-sm peer-hover:scale-105 peer-hover:bg-neutral-300 peer-hover:text-neutral-900",
+                      {
+                        "bg-green-200 peer-hover:bg-green-300/80":
+                          item.isSelected,
+                      },
+                    )}
                     size={"lg"}
                   >
                     {item.isSelected ? (
-                      <IoCheckmarkDoneCircleSharp size={30} />
+                      <IoCheckmarkDoneCircleSharp size={30} className="" />
                     ) : (
-                      <IoCheckmarkDoneCircleOutline size={30} />
+                      <IoCheckmarkDoneCircleOutline size={30} className="" />
                     )}
                   </Button>
                 </label>
 
-                <div className="h-0.5 w-[80%] rounded-full bg-black/40" />
-                <Button
-                  variant="ghost"
-                  onClick={() => handleRemove(item.id)}
-                  className="flex-1 rounded-full p-0 shadow-sm"
-                  size={"lg"}
-                >
-                  <RiDeleteBin6Line size={30} />
-                </Button>
+                <div className="h-0.5 w-[100%] rounded-full bg-black/10" />
+
+                <div className="flex flex-1 items-center justify-center">
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="flex h-[3.5rem] w-[3.5rem] items-center justify-center p-0 text-rose-500 shadow-sm hover:scale-105 hover:bg-rose-300 hover:text-rose-800"
+                        size={"lg"}
+                      >
+                        <RiDeleteBin6Line size={30} />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                      <DialogHeader>
+                        <DialogTitle>Are you sure?</DialogTitle>
+                        <DialogDescription>
+                          Delete{" "}
+                          <span className="font-semibold">
+                            {item.productTitle}
+                          </span>{" "}
+                          from my cart.
+                        </DialogDescription>
+                      </DialogHeader>
+
+                      <DialogFooter>
+                        <Button
+                          type="submit"
+                          variant="ghost"
+                          onClick={() => handleRemove(item.id)}
+                          className="flex-1 rounded-full bg-rose-300 p-0 shadow-sm hover:bg-rose-400"
+                          size={"lg"}
+                        >
+                          Delete
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+                </div>
               </div>
 
               <div className="col-span-2 max-w-[10rem] overflow-hidden rounded-md">
@@ -129,14 +204,13 @@ export default function CartPage() {
                 </div>
 
                 <Text size="md" className="">
-                  Price:
-                  <span className="text-lg font-semibold">$ {item.price}</span>
+                  Price: <span className="text-base">${item.price}</span>
                 </Text>
               </div>
-              <div className="col-span-2">
-                <div className="flex items-center justify-center gap-2">
-                  <Text>Quantity</Text>
-                  <div className="flex h-14 items-center justify-center gap-5 rounded-md border border-black p-2">
+              <div className="col-span-2 space-y-4">
+                <div className="flex items-center justify-between gap-2">
+                  <Text>Quantity:</Text>
+                  <div className="flex h-14 items-center justify-center gap-5 rounded-md border border-black/40 p-2">
                     <Button
                       onClick={() => handleDecrease(item.id)}
                       className="flex h-8 w-8 items-center justify-center rounded-full p-2"
@@ -154,9 +228,11 @@ export default function CartPage() {
                     </Button>
                   </div>
                 </div>
-                <div className="flex items-center justify-center gap-2">
+                <div className="flex items-center justify-between gap-2">
                   <Text>Total price:</Text>
-                  <Text>{item.price * item.quantity}</Text>
+                  <Text size="lg" className="font-semibold">
+                    ${item.price * item.quantity}
+                  </Text>
                 </div>
               </div>
             </div>
@@ -168,17 +244,6 @@ export default function CartPage() {
             Total for selected items
           </Heading>
           <div className="flex w-full flex-col items-center justify-start gap-5">
-            <div className="flex w-full flex-row items-center justify-between py-0.5">
-              <Text
-                size="lg"
-                className="!font-raleway tracking-[-0.50px] !text-gray-500"
-              >
-                Subtotal
-              </Text>
-              <Heading className="!font-poppins tracking-[-0.50px]">
-                $ 99.00
-              </Heading>
-            </div>
             <div className="flex w-full flex-row items-center justify-center">
               <input
                 name="your_voucher"
@@ -207,6 +272,6 @@ export default function CartPage() {
           </Button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
