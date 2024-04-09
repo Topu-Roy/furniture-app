ba472db64424cef1a9f9a4809987accd802a050d
Topu-Roy <topuroy143333@gmail.com>
2024-04-10 00:45:04 +0600

    UI - Home hero section responsive

--- src/app/home/home/heroSection.tsx
+++ src/app/home/home/heroSection.tsx
@@ -1,41 +1,63 @@
 import React from "react";
-import { Text } from "@/components";
+import { Heading, Text } from "@/components";
 import { Button } from "@/components/ui/button";
 import { poppins } from "@/styles/font";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <div className="w-full bg-orange-50 py-10">
-      <div className="mx-auto w-[80rem]">
-        <div className="my-[5px] flex w-full max-w-[1290px] flex-row items-center justify-between">
-          <div className="flex w-[48%] flex-col items-start justify-start gap-[30px]">
-            <div className="flex w-full flex-col items-start justify-start gap-[26px]">
-              <Text size="xl" className="!text-black-900 tracking-[-0.50px]">
+    <div className="mt-[5rem] w-full py-8 md:py-10">
+      <div className="mx-auto w-full max-w-7xl">
+        <div className="my-[5px] flex w-full max-w-[1290px] flex-col-reverse items-center justify-between px-2 md:flex-row">
+          <div className="flex w-full flex-col items-start justify-start gap-[30px] md:w-[48%]">
+            <div className="flex w-full flex-col items-start justify-start gap-2 md:justify-start">
+              <Text
+                size="lg"
+                className={`${poppins.className} mt-4 tracking-[-0.50px] text-black/60 md:text-start`}
+              >
                 Interior Needs...
               </Text>
               <Text
                 size="max"
-                className={`!text-black-900 tracking-normal ${poppins.className}`}
+                className={`${poppins.className} hidden text-start tracking-normal text-gray-900/80 md:block`} //* For bigger screens
               >
                 Various new collections of furniture to decorate the corner of
                 your house.
               </Text>
+              <Heading
+                size="2xl"
+                className={`${poppins.className} tracking-normal text-gray-900/80 md:hidden`} //* For smaller screens
+              >
+                Various new collections of furniture to decorate the corner of
+                your house.
+              </Heading>
             </div>
-            <Button
-              size="9xl"
-              className="min-w-[218px] border-2 border-solid  font-medium tracking-[-0.50px]"
-            >
-              Shop Now
-            </Button>
+            <Link href={"/shop"}>
+              <Button
+                size="9xl"
+                className="border-2 border-solid font-medium tracking-[-0.50px]"
+              >
+                Shop Now
+              </Button>
+            </Link>
           </div>
-          <Image
-            height={1000}
-            width={1000}
-            src="/images/img_nathan_oakley_o.png"
-            alt="nathanoakleyo"
-            className="w-[48%] object-cover"
-          />
+          <div className="relative h-[90dvw] w-[95dvw] rounded-lg sm:h-[70dvw]  sm:w-[75dvw] md:h-[475px] md:w-[48%]">
+            <Image
+              height={1000}
+              width={1000}
+              src="/hero/Lamp - Black - Vintage Desk Lamp (1).jpg"
+              alt=""
+              className="absolute left-0 top-0 w-[60%] rounded-lg bg-slate-600 object-cover"
+            />
+            <Image
+              height={1000}
+              width={1000}
+              src="/hero/Bed - Brown - Four Poster Canopy Bed (2).jpg"
+              alt=""
+              className="absolute bottom-0 right-0 w-[60%] rounded-lg object-cover shadow-2xl"
+            />
+          </div>
         </div>
       </div>
     </div>
