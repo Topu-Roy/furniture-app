af819b6d784e743275b2097da11e61a0a2173fc9
Topu-Roy <topuroy143333@gmail.com>
2024-04-09 12:33:29 +0600

    UI - Font from layout is moved to the font file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from "@/styles/font";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,10 +1,16 @@
-import { Dosis, Poller_One, Poppins } from "next/font/google";
+import { Dosis, Inter, Poller_One, Poppins } from "next/font/google";
 
 export const pollerOne = Poller_One({
   weight: ["400"],
   subsets: ["latin"],
   display: "swap",
 });
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: 'swap'
+}
+);
 
 export const poppins = Poppins({
   weight: '600',
