6ad2aa87dd0dfde6eeb60fba1bcc32da70b73509
Topu-Roy <topuroy143333@gmail.com>
2024-04-13 01:49:09 +0600

    UI - Home -> Hero fully responsive

--- src/app/home/home/heroSection.tsx
+++ src/app/home/home/heroSection.tsx
@@ -1,54 +1,49 @@
 import React from "react";
-import { Heading, Text } from "@/components";
+import { Text } from "@/components";
 import { Button } from "@/components/ui/button";
 import { poppins } from "@/styles/font";
 import Image from "next/image";
 import Link from "next/link";
+import { BiShoppingBag } from "react-icons/bi";
 
 export default function HeroSection() {
   return (
-    <div className="mt-[5rem] w-full py-8 md:py-10">
+    <div className="mt-[5rem] w-full py-8 md:py-10 lg:px-4">
       <div className="mx-auto w-full max-w-7xl">
         <div className="my-[5px] flex w-full max-w-[1290px] flex-col-reverse items-center justify-between px-2 md:flex-row">
-          <div className="flex w-full flex-col items-start justify-start gap-[30px] md:w-[48%]">
-            <div className="flex w-full flex-col items-start justify-start gap-2 md:justify-start">
+          <div className="flex w-full flex-col items-center justify-start gap-[30px] sm:px-4 md:w-[50%] md:items-start md:px-0">
+            <div className="flex w-full flex-col items-center justify-start gap-2 pt-6 md:items-start md:justify-start">
               <Text
-                size="lg"
-                className={`${poppins.className} mt-4 tracking-[-0.50px] text-black/60 md:text-start`}
+                size="s"
+                className={`${poppins.className} rounded-full bg-secondary p-1 px-2 tracking-[-0.50px] shadow-md ring-[3px] ring-secondary sm:mb-4`}
               >
-                Interior Needs...
+                Interior Needs
               </Text>
-              <Text
-                size="max"
-                className={`${poppins.className} hidden text-start tracking-normal text-gray-900/80 md:block`} //* For bigger screens
+              <h1
+                className={`${poppins.className} text-center text-3xl tracking-normal text-gray-900/80 
+                md:pr-1 md:text-left md:text-4xl lg:pr-3 xl:pr-14 xl:text-5xl`}
               >
                 Various new collections of furniture to decorate the corner of
                 your house.
-              </Text>
-              <Heading
-                size="2xl"
-                className={`${poppins.className} tracking-normal text-gray-900/80 md:hidden`} //* For smaller screens
-              >
-                Various new collections of furniture to decorate the corner of
-                your house.
-              </Heading>
+              </h1>
             </div>
             <Link href={"/shop"}>
               <Button
                 size="9xl"
-                className="border-2 border-solid font-medium tracking-[-0.50px]"
+                className="flex items-center justify-center gap-2 border-2 border-solid font-medium tracking-[-0.50px]"
               >
-                Shop Now
+                <p>Shop Now</p>
+                <BiShoppingBag size={24} />
               </Button>
             </Link>
           </div>
-          <div className="relative h-[90dvw] w-[95dvw] rounded-lg md:h-[23rem] md:w-[25rem] xl:h-[30rem] xl:w-[35rem]">
+          <div className="lg:w-[26rem relative h-[80dvw] w-[95dvw] rounded-lg sm:mb-8 sm:h-[23rem] sm:w-[30rem] md:h-[25rem] md:w-[25rem] lg:h-[30rem] lg:w-[30rem]">
             <Image
               height={1000}
               width={1000}
               src="/hero/Lamp - Black - Vintage Desk Lamp (1).jpg"
               alt=""
-              className="absolute left-0 top-0 w-[60%] rounded-lg bg-slate-600 object-cover"
+              className="absolute left-0 top-0 w-[60%] rounded-lg object-cover"
             />
             <Image
               height={1000}
