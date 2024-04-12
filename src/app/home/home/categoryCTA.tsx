c6d1f4d41506dea006e2dfb4ae71ec8c9651a5a1
Topu-Roy <topuroy143333@gmail.com>
2024-04-13 03:15:26 +0600

    UI - Added aceternity ui

--- src/app/home/home/categoryCTA.tsx
+++ src/app/home/home/categoryCTA.tsx
@@ -1,68 +1,34 @@
-import { Heading, Text } from "@/components";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import React from "react";
+import { products } from "@/assets/productArray";
+import ThreeDCard from "@/components/3d/ThreeDCard";
+import { Heading, Text } from "@/components";
+import { cn } from "@/lib/utils";
 
 export default function CategoryCTA() {
+  const chair = products.filter((item) => item.category === "Chair")[2];
+  const lamp = products.filter((item) => item.category === "Lamp")[4];
   return (
-    <div className="w-full py-20">
-      <div className="mx-auto flex w-full max-w-[1290px] flex-row gap-5">
-        <div className="flex w-full flex-row items-center justify-between bg-gradient p-6">
-          <div className="ml-1.5 flex w-[55%] flex-col items-start justify-start gap-6">
-            <div className="flex w-full flex-col items-start justify-start gap-7">
-              <Text size="md" className="tracking-[-0.50px] !text-gray-50_01">
-                Living Room
-              </Text>
-              <Heading
-                size="3xl"
-                className="tracking-[-0.50px] !text-gray-50_01"
-              >
-                The best foam padded chair
-              </Heading>
-            </div>
-            <Button
-              size="6xl"
-              className="min-w-[155px] font-medium tracking-[-0.50px]"
-            >
-              Shop Now
-            </Button>
-          </div>
-          <Image
-            height={500}
-            width={500}
-            src="/images/img_sam_moghadam_kh.png"
-            alt="image"
-            className="mr-1.5 w-[31%] object-cover"
-          />
-        </div>
-        <div className="flex w-full flex-row items-center justify-between bg-gradient p-[30px]">
-          <div className="flex w-[54%] flex-col items-start justify-start gap-6">
-            <div className="flex w-full flex-col items-start justify-start gap-7">
-              <Text size="md" className="tracking-[-0.50px] !text-gray-50_01">
-                Living Room
-              </Text>
-              <Heading
-                size="3xl"
-                className="tracking-[-0.50px] !text-gray-50_01"
-              >
-                Latest model chandelier
-              </Heading>
-            </div>
-            <Button
-              size="6xl"
-              className="min-w-[155px] font-medium tracking-[-0.50px]"
-            >
-              Shop Now
-            </Button>
-          </div>
-          <Image
-            height={500}
-            width={500}
-            src="/images/img_phil_desforges.png"
-            alt="phildesforges"
-            className="my-[22px] w-[34%] object-cover"
-          />
-        </div>
+    <div className="mx-auto w-full max-w-7xl py-14">
+      <Heading size="3xl" className={cn("text-center text-gray-900")}>
+        Find a variety of home furniture's
+      </Heading>
+      <div className="flex w-full items-center justify-center gap-8">
+        <ThreeDCard
+          heading="Try new our chairs"
+          subHeading="Brand new collection of chairs and modern design."
+          imageURL={chair.image}
+          imageAlt={chair.productTitle}
+          link="/shop"
+          linkName="Shop Now"
+        />
+        <ThreeDCard
+          heading="Check out our new Lamps"
+          subHeading="Brand new collection of Lamps with minimal design."
+          imageURL={lamp.image}
+          imageAlt={lamp.productTitle}
+          link="/shop"
+          linkName="Shop Now"
+        />
       </div>
     </div>
   );
